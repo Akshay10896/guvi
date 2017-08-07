de73c00a47e663743b1a579e31eb6bffe8cba40b
Akshay10896 <30789402+Akshay10896@users.noreply.github.com>
2017-08-07 09:57:20 +0530

    Update and rename vj.c to 1g.cpp

---
+++ 1g.cpp
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+signed long a;
+clrscr();
+cin>>a;
+if(a>0)
+{
+cout<<"positive";
+}
+else if(a<0)
+{
+cout<<"negative";
+}
+else
+{
+cout<<"zero";
+}
+getch();
+}
--- vj.c
+++
@@ -1,1 +0,0 @@
-bunijmmo,kmjnbk
